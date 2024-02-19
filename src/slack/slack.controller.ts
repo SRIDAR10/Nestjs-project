31d40fb405ced94a089e434d69738f4535aebf16
Sridar Saikrishnan <sridar.krishnan@preskale.com>
2024-02-19 14:04:55 +0530

    changes

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -51,6 +51,8 @@ export class SlackController {
     try {
       Logger.log(`slash command payload type: ${typeof payload}`);
       Logger.log(`slash command payload content: ${JSON.stringify(payload)} token => ${payload?.token}`);
+      Logger.log(payload);
+      Logger.log(payload?.trigger_id);
      Logger.log("triggerId ==============================>", payload?.trigger_id, payload.user_name, payload["trigger_id"]);
       await this.sendInitialModalView(payload?.trigger_id);
       res.status(200);
