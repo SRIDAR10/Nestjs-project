f07003d67e11bee94520e1aca6e44db4b25cb199
Sridar Saikrishnan <sridar.krishnan@preskale.com>
2024-02-21 23:11:41 +0530

    changes

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -136,52 +136,146 @@ export class SlackController {
         throw new Error('Missing user ID in payload');
       }
   const modal = {
-    "title": {
-      "type": "plain_text",
-      "text": "My App",
-      "emoji": true
-    },
+    "type": "modal",
     "submit": {
       "type": "plain_text",
       "text": "Submit",
       "emoji": true
     },
-    "type": "modal",
     "close": {
       "type": "plain_text",
       "text": "Cancel",
       "emoji": true
     },
+    "title": {
+      "type": "plain_text",
+      "text": "App menu",
+      "emoji": true
+    },
     "blocks": [
       {
-        "type": "input",
-        "element": {
-          "type": "plain_text_input",
-          "action_id": "option_1",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "First option"
-          }
-        },
-        "label": {
-          "type": "plain_text",
-          "text": "Option 1"
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "*Hi <fakelink.toUser.com|@David>!* Here's how I can help you:"
         }
       },
       {
-        "type": "input",
-        "element": {
-          "type": "plain_text_input",
-          "action_id": "title",
+        "type": "divider"
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": ":calendar: *Create event*\nCreate a new event"
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Create event",
+            "emoji": true
+          },
+          "style": "primary",
+          "value": "click_me_123"
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": ":clipboard: *List of events*\nChoose from different event lists"
+        },
+        "accessory": {
+          "type": "static_select",
           "placeholder": {
             "type": "plain_text",
-            "text": "What do you want to ask of the world?"
-          }
-        },
-        "label": {
-          "type": "plain_text",
-          "text": "Title"
+            "text": "Choose list",
+            "emoji": true
+          },
+          "options": [
+            {
+              "text": {
+                "type": "plain_text",
+                "text": "My events",
+                "emoji": true
+              },
+              "value": "value-0"
+            },
+            {
+              "text": {
+                "type": "plain_text",
+                "text": "All events",
+                "emoji": true
+              },
+              "value": "value-1"
+            },
+            {
+              "text": {
+                "type": "plain_text",
+                "text": "Event invites",
+                "emoji": true
+              },
+              "value": "value-1"
+            }
+          ]
         }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": ":gear: *Settings*\nManage your notifications and team settings"
+        },
+        "accessory": {
+          "type": "static_select",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Edit settings",
+            "emoji": true
+          },
+          "options": [
+            {
+              "text": {
+                "type": "plain_text",
+                "text": "Notifications",
+                "emoji": true
+              },
+              "value": "value-0"
+            },
+            {
+              "text": {
+                "type": "plain_text",
+                "text": "Team settings",
+                "emoji": true
+              },
+              "value": "value-1"
+            }
+          ]
+        }
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Send feedback",
+              "emoji": true
+            },
+            "value": "click_me_123"
+          },
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "FAQs",
+              "emoji": true
+            },
+            "value": "click_me_123"
+          }
+        ]
       }
     ]
   }
